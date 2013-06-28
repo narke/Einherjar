e08672b06598e258fce1a4e6dbca059cf7445a9c
Konstantin Tcholokachvili <tcholoka@gmail.com>
2013-06-29 01:09:25 +0200

    Converted the multiboot code from GAS to NASM.

--- bootloader/multiboot.asm
+++ bootloader/multiboot.asm
@@ -1,47 +1,47 @@
-# @see http://wiki.osdev.org/Bare_Bones
-# Slightly modified by me
+; @see http://wiki.osdev.org/Bare_Bones
+; Slightly modified by me
 
-.global multiboot_entry
+extern roentgenium_main	; this is our kernel's entry point
 
-# setting up the Multiboot header - see GRUB docs for details
-.set ALIGN,    1<<0                     # align loaded modules on page boundaries
-.set MEMINFO,  1<<1                     # provide memory map
-.set FLAGS,    ALIGN | MEMINFO          # this is the Multiboot 'flag' field
-.set MAGIC,    0x1BADB002               # 'magic number' lets bootloader find the header
-.set CHECKSUM, -(MAGIC + FLAGS)         # checksum required
-.set STACK_SIZE, 0x4000			# our stack size is 16KiB
+; setting up the Multiboot header - see GRUB docs for details
+MBALIGN 	equ  1<<0		; align loaded modules on page boundaries
+MEMINFO		equ  1<<1		; provide memory map
+FLAGS		equ  MBALIGN | MEMINFO	; this is the Multiboot 'flag' field
+MAGIC		equ  0x1BADB002		; 'magic number' lets bootloader find the header
+CHECKSUM	equ  -(MAGIC + FLAGS)	; checksum required to prove that we are multiboot
+STACK_SIZE	equ  0x4000		; our stack size is 16KiB
 
 
-# The multiboot header must come first.
-.section ".multiboot"
+; The multiboot header must come first.
+section .multiboot
 
-# Multiboot header must be aligned on a 4-byte boundary
-.align 4
+; Multiboot header must be aligned on a 4-byte boundary
+align 4
 
 multiboot_header:
-.long MAGIC
-.long FLAGS
-.long -(MAGIC + FLAGS)
+dd MAGIC
+dd FLAGS
+dd -(MAGIC + FLAGS)
 
-# The beginning of our kernel code
-.text
+; The beginning of our kernel code
+section .text
 
-
+global multiboot_entry
 multiboot_entry:
-	movl $(stack + STACK_SIZE), %esp	# set up the stack
-	movl  %eax, magic			# Multiboot magic number
-	movl  %ebx, mbd				# Multiboot data structure
+	mov esp, stack + STACK_SIZE	; set up the stack
+	mov [magic], eax		; multiboot magic number
+	mov [multiboot_info], ebx	; multiboot data structure
 
-	call roentgenium_main			# calling the kernel
+	call roentgenium_main		; calling the kernel
 
 hang:
-	hlt					# something bad happened, machine halted
+	hlt				; something bad happened, machine halted
 	jmp hang
 
 
 
-# reserve initial kernel stack space
+; reserve initial kernel stack space
 stack:
-.skip STACK_SIZE	# reserve 16 KiB stack
-.comm  mbd, 4		# we will use this in kernel's main
-.comm  magic, 4		# we will use this in kernel's main
+resb STACK_SIZE		; reserve 16 KiB stack
+multiboot_info: resd 1	; we will use this in kernel's main
+magic: resd 1		; we will use this in kernel's main
