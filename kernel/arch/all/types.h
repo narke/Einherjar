64707d2d45d057d1ecf270b136f9064487294a1a
Konstantin Tcholokachvili <tcholoka@gmail.com>
2013-08-19 11:08:30 +0200

    Added size_t and off_t definitions

--- kernel/arch/all/types.h
+++ kernel/arch/all/types.h
@@ -14,16 +14,16 @@
 
 typedef enum { FALSE=0, TRUE } bool_t;
 
-typedef unsigned long long int  uint64_t;
-typedef unsigned long int       uint32_t; 	/* 32 bits unsigned */
-typedef unsigned short int      uint16_t; 	/* 16 bits unsigned */
-typedef unsigned char           uint8_t;  	/* 8  bits unsigned */
-typedef unsigned char           uchar_t;  	/* 8  bits unsigned */
+typedef unsigned long long int  uint64_t;	/**< 64 bits unsigned */
+typedef unsigned long int       uint32_t; 	/**< 32 bits unsigned */
+typedef unsigned short int      uint16_t; 	/**< 16 bits unsigned */
+typedef unsigned char           uint8_t;  	/**< 8  bits unsigned */
+typedef unsigned char           uchar_t;  	/**< 8  bits unsigned */
 
-typedef signed long long int    sint64_t;	/* 64 bits signed */
-typedef signed long int         sint32_t; 	/* 32 bits signed */
-typedef signed short int        sint16_t; 	/* 16 bits signed */
-typedef signed char             sint8_t;	/* 8  bits signed */
+typedef signed long long int    sint64_t;	/**< 64 bits signed */
+typedef signed long int         sint32_t; 	/**< 32 bits signed */
+typedef signed short int        sint16_t; 	/**< 16 bits signed */
+typedef signed char             sint8_t;	/**< 8  bits signed */
 
 /** Physical address */
 typedef unsigned int		paddr_t;
@@ -31,6 +31,11 @@ typedef unsigned int		paddr_t;
 /** Virtual address */
 typedef unsigned int		vaddr_t;
 
+/** Memory size of an object */
+typedef unsigned int		size_t;
+
+/** Offset type */
+typedef signed int		off_t;
 
 #endif // _TYPES_H_
 
